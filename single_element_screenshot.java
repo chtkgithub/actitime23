39c3fd5b356516486209dff565b8ee8f87612ca4
chtkgithub <126074338+chtkgithub@users.noreply.github.com>
2023-03-24 09:33:07 -0700

    Add files via upload

---
+++ single_element_screenshot.java
@@ -0,0 +1,51 @@
+package basic;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class single_element_screenshot {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+	   	 LocalDateTime ldt = LocalDateTime.now();
+	   	 
+	     String timestamp = ldt.toString().replace(':','-' );
+	     
+	     
+	  System.setProperty("webdriver.gecko.driver", "E:\\firefox\\driver\\geckodriver.exe");
+	       
+			
+			WebDriver driver = new FirefoxDriver();
+			
+			
+			driver.get("https://www.irctc.com/contact.html");
+			
+			
+			
+			
+			
+		WebElement button = driver.findElement(By.xpath("//h2[text()='Contact Us']"));
+		
+	File temfile = button.getScreenshotAs(OutputType.FILE);
+	
+	File destfile = new File("./webpage/screenshot1"+timestamp+".png");
+	
+	  FileUtils.copyFile(temfile, destfile); 
+	  
+		
+		
+		
+		
+
+	}
+
+}
